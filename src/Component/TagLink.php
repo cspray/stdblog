3d7e3b34fb3b874cf6edff0c309316352d9d46ce
Charles Sprayberry <me@cspray.io>
2021-05-30 17:14:43 -0400

    add tag link component

---
+++ src/Component/TagLink.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Cspray\StdBlog\Component;
+
+class TagLink extends AbstractComponent {
+
+    private string $name;
+    private string $href;
+
+    public function __construct(string $name, string $href) {
+        $this->name = $name;
+    }
+
+    public function render() {
+        return sprintf(
+            '<a href="%s"><span class="py-0.5 px-2 border rounded-md text-xs">%s</span></a>',
+            self::escaper()->escapeHtmlAttr($this->href),
+            self::escaper()->escapeHtml($this->name)
+        );
+    }
+}
