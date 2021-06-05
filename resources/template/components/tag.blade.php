9a00c78ca7439c06b2a325f63e22950df925aafa
Charles Sprayberry <me@cspray.io>
2021-06-05 15:17:27 -0400

    Improve tag display when it is a link

--- resources/template/components/tag.blade.php
+++ resources/template/components/tag.blade.php
@@ -1,5 +1,5 @@
 @if(isset($href))
-<a href="{{ $href }}">
+<a class="flex" href="{{ $href }}">
 @endif
    <span class="py-0.5 px-2 border rounded-md text-xs {{ $class }}" >{{ $name }}</span>
 @if(isset($href))
