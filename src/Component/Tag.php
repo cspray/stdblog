dfa4906949385e827d69bbf46473232682a65617
Charles Sprayberry <me@cspray.io>
2021-06-05 09:51:37 -0400

    Consolidate tag link and tag into one

--- src/Component/Tag.php
+++ src/Component/Tag.php
@@ -5,19 +5,24 @@ namespace Cspray\StdBlog\Component;
 class Tag extends AbstractComponent {
 
     private string $name;
-
     private string $class;
+    private ?string $href = null;
 
-    public function __construct(string $name, string $class = '') {
+    public function __construct(string $name, string $href = null, string $class = '') {
         $this->name = $name;
         $this->class = $class;
+        $this->href = $href;
     }
 
     public function render() {
-        return sprintf(
-            '<span class="py-0.5 px-2 border rounded-md text-xs %s">%s</span>',
-            self::escaper()->escapeHtmlAttr($this->class),
-            self::escaper()->escapeHtml($this->name)
-        );
+        return <<<'blade'
+@if(isset($href))
+<a href="{{ $href }}">
+@endif
+    <span class="py-0.5 px-2 border rounded-md text-xs {{ $class }}">{{ $name }}</span>
+@if(isset($href))
+</a>
+@endif
+blade;
     }
 }
