905898467379a0e1a797d65220a6e6fef5e33ebc
Charles Sprayberry <me@cspray.io>
2021-06-05 04:26:22 -0400

    Allow customization of the tag component

--- src/Component/Tag.php
+++ src/Component/Tag.php
@@ -6,13 +6,17 @@ class Tag extends AbstractComponent {
 
     private string $name;
 
-    public function __construct(string $name) {
+    private string $class;
+
+    public function __construct(string $name, string $class = '') {
         $this->name = $name;
+        $this->class = $class;
     }
 
     public function render() {
         return sprintf(
-            '<span class="py-0.5 px-2 border rounded-md text-xs">%s</span>',
+            '<span class="py-0.5 px-2 border rounded-md text-xs %s">%s</span>',
+            self::escaper()->escapeHtmlAttr($this->class),
             self::escaper()->escapeHtml($this->name)
         );
     }
